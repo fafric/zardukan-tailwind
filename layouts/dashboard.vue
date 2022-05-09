ac9b9a64157820be00a887e04df2b7c89281f670
Suleman Amin <fafric@gmail.com>
2022-05-09 09:35:14 +0500

    test 1

---
+++ layouts/dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav-bar @menu="userManue = !userManue" :mydropmenu="userManue" />
+    <main>
+      <div class="flex flex-col md:flex-row">
+        <side-bart />
+        <section class="container mx-auto pt-8">
+           
+                <div class="bg-gray-800 pt-6">
+                      <div class="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
+                          <h1 class="font-bold pl-2">{{$route.name}}</h1>
+                      </div>
+                </div>
+                 <div class="flex flex-wrap">
+                    <div class="w-full  xl:w-1/3 p-6">
+                       <Nuxt />
+                    </div>
+                 </div>
+           
+        </section>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+      return {
+        userManue: false
+      }
+    },
+}
+</script>
+
+<style>
+ @import url("https://use.fontawesome.com/releases/v5.3.1/css/all.css");
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>hello this is index page</h1>
+</template>
+
+<script>
+
+export default {
+  layout:'dashboard',
+  name: 'IndexPage',
+  data() {
+      return {
+       //
+      }
+    },
+  methods: {
+   //
+  }
+}
+// https://github.com/tailwindtoolbox/Admin-Template/blob/master/index.html
+</script>
+<style scoped>
+
+</style>
